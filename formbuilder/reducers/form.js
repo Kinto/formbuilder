592e64f7574b56c5943aa14b64b40e34968ac073
Nicolas Perriault <nperriault@gmail.com>
2016-01-09 10:56:02 +0100

    Added support for reorderable fields.

--- formbuilder/reducers/form.js
+++ formbuilder/reducers/form.js
@@ -1,13 +1,23 @@
 import {
   FIELD_ADD,
   FIELD_REMOVE,
-  FIELD_UPDATE
+  FIELD_UPDATE,
+  FIELD_MOVE,
 } from "../actions/fieldlist";
 
 const INITIAL_STATE = {};
 
 function clone(obj) {
   return JSON.parse(JSON.stringify(obj));
+}
+
+function moveInArray(array, from, to) {
+  const arrayCopy = [].slice.call(array);
+  if (to === from) {
+    return arrayCopy;
+  }
+  arrayCopy.splice(to, 0, arrayCopy.splice(from, 1)[0]);
+  return arrayCopy;
 }
 
 function generateUniqueFieldName(names) {
@@ -21,8 +31,9 @@ function generateUniqueFieldName(names) {
 function addField(state, field) {
   // Generating a usually temporary random, unique field name.
   const name = generateUniqueFieldName(Object.keys(state.schema.properties));
-  state.schema.properties[name] = field.jsonSchema;
+  state.schema.properties[name] = {...field.jsonSchema, title: name};
   state.uiSchema[name] = field.uiSchema;
+  state.uiSchema.order = (state.uiSchema.order || []).concat(name);
   state.editSchema[name] = field.editSchema;
   return state;
 }
@@ -31,6 +42,8 @@ function removeField(state, name) {
   const requiredFields = state.schema.required || [];
   delete state.schema.properties[name];
   delete state.uiSchema[name];
+  state.uiSchema.order = state.uiSchema.order.filter(
+    (field) => field !== name);
   delete state.editSchema[name];
   state.schema.required = requiredFields
     .filter(requiredFieldName => name !== requiredFieldName);
@@ -49,6 +62,17 @@ function updateField(state, name, schema, required) {
   return state;
 }
 
+function moveField(state, name, direction) {
+  const fields = Object.keys(state.schema.properties);
+  const order = state.uiSchema.order || fields;
+  const from = order.indexOf(name);
+  const to = direction === "up" && from > 0 ? from - 1 :
+             direction === "down" && from < order.length - 1 ? from + 1 :
+             from;
+  state.uiSchema.order = moveInArray(order, from, to);
+  return state;
+}
+
 export default function form(state = INITIAL_STATE, action) {
   switch(action.type) {
   case FIELD_ADD:
@@ -57,6 +81,8 @@ export default function form(state = INITIAL_STATE, action) {
     return removeField(clone(state), action.name);
   case FIELD_UPDATE:
     return updateField(clone(state), action.name, action.schema, action.required);
+  case FIELD_MOVE:
+    return moveField(clone(state), action.name, action.direction);
   default:
     return state;
   }
