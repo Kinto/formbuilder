c1ed111efa0336ef449b7b03744d5f0a3fed06b8
Alexis Métaireau <alexis@notmyidea.org>
2016-04-15 23:02:21 +0200

    Rename the fieldList prop to sidebarComponent.
    Fix #28

--- formbuilder/containers/App.js
+++ formbuilder/containers/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 export default function App(props) {
-  const {fieldList, content, notifications, header} = props;
-  const contentClassName = fieldList? "col-sm-9" : "col-sm-9 center";
+  const {sidebarComponent, content, notifications, header} = props;
+  const contentClassName = sidebarComponent? "col-sm-9" : "col-sm-9 center";
   return (
     <div>
       {header}
       <div className="container">
         <div className="row">
-          {fieldList ? <div className="col-sm-3">{fieldList}</div> : <div/>}
+          {sidebarComponent ? <div className="col-sm-3">{sidebarComponent}</div> : <div/>}
           <div className={contentClassName}>
             {notifications}
             {content || <p>Nothing to render</p>}
