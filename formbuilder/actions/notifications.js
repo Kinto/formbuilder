8eb26b8a0929dd1a51db9e3ce9b71de4b715bfcc
Alexis Métaireau <alexis@notmyidea.org>
2016-04-27 11:52:18 +0200

    Add tests for the notifications

--- formbuilder/actions/notifications.js
+++ formbuilder/actions/notifications.js
@@ -3,19 +3,18 @@ import uuid from "uuid";
 export const NOTIFICATION_ADD = "NOTIFICATION_ADD";
 export const NOTIFICATION_REMOVE = "NOTIFICATION_REMOVE";
 
-export function addNotification(message, type="info", autoDismiss=null) {
-  if (type=="error" && autoDismiss == null) {
-    autoDismiss = false;
-  } else {
-    autoDismiss = true;
+export function addNotification(message, type="info", autoDismiss=null, dismissAfter=5000) {
+  if (autoDismiss == null) {
+    autoDismiss = (type=="error") ? false : true;
   }
   return (dispatch) => {
     const id = uuid.v4();
     dispatch({type: NOTIFICATION_ADD, notification: {id, message, type}});
+    console.log("AutoDismiss", autoDismiss);
     if (autoDismiss == true) {
       setTimeout(() => {
         dispatch({type: NOTIFICATION_REMOVE, id});
-      }, 5000);
+      }, dismissAfter);
     }
   };
 }
