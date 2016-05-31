31210aa94083f51122d56f33e1d773a393161514
Alexis Métaireau <alexis@notmyidea.org>
2016-05-31 02:35:25 +0200

    Do some styling

--- formbuilder/components/Welcome.js
+++ formbuilder/components/Welcome.js
@@ -23,18 +23,18 @@ export default function Welcome(props) {
       <div className="container">
         <div className="row">
         <div className="col-md-4">
-          <h2><i className="glyphicon glyphicon-eye-close"></i> Your privacy matters</h2>
+          <h3><i className="glyphicon glyphicon-eye-close"></i> Privacy matters</h3>
           <p>With <a href="https://kinto-storage.org">Kinto</a>, you are not giving Google or any other giants your data.</p>
           <p>Our goal is not to host all the forms of the world, so we try to make it easy for you to <a href="http://kinto.readthedocs.org/en/latest/get-started.html#get-started">host your own servers</a>.</p>
         </div>
         <div className="col-md-4">
-          <h2><i className="glyphicon glyphicon-heart-empty"></i> Community focused</h2>
+          <h3><i className="glyphicon glyphicon-heart-empty"></i> Open source</h3>
           <p>All the code we write is <a href="https://github.com/kinto/formbuilder">written in the open</a> and we try to be
           the most inclusive as we can to welcome your ideas.</p>
           <p>Kinto and the formbuilder are released under Apache 2.0 licenses</p>
         </div>
         <div className="col-md-4">
-          <h2><i className="glyphicon glyphicon-saved"></i> Account-less</h2>
+          <h3><i className="glyphicon glyphicon-saved"></i> Account-less</h3>
           <p>You don't need an account to create a new form: just create and give it to your friends, it's a matter of minutes!</p>
         </div>
         </div>
