b3c9a3039e7e8a9a1e989474ce6b0ecb71f87bbe
Alexis Métaireau <alexis@notmyidea.org>
2016-06-07 00:30:39 +0200

    Add a background image to the welcome screen

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -57,6 +57,13 @@ module.exports = {
           path.join(__dirname, "formbuilder"),
           path.join(__dirname, "node_modules"),
         ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          "file?hash=sha512&digest=hex&name=[hash].[ext]",
+          "image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false"
+        ]
       }
     ]
   }
