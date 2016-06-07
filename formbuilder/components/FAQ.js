ed76a6345380d04ffcf945d59795d5c5b2b2b302
Alexis Métaireau <alexis@notmyidea.org>
2016-06-07 10:22:15 +0200

    Mention the IRC channel as well (thanks @DirtyF)

--- formbuilder/components/FAQ.js
+++ formbuilder/components/FAQ.js
@@ -14,7 +14,8 @@ export default function FAQ(props) {
       <h3>I found a bug, where can I report it?</h3>
       <p>We are currently using Github to manage issues.
       If you want, you can open an issue <a href="https://www.github.com/kinto/formbuilder/issues">on our bugtracker</a>.
-      If you prefer, you can also <a href="mailto:contact@fourmilieres.net">send us an email</a>.</p>
+      You can also <a href="mailto:contact@fourmilieres.net">send us an email</a> or come talk with us on our IRC channel <code>#kinto</code> on <code>irc.freenode.net</code> —
+      (<a href="https://kiwiirc.com/client/irc.freenode.net/?#kinto">Click here to access the web client</a>).</p>
     </div>
   );
 }
