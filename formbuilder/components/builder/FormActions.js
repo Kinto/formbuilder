92dae6015a3d8ac62fa1138d0506e3dd5c90d0ad
Romain Le Cellier <rlecellier@gmail.com>
2016-09-15 16:26:55 +0200

    issus 75 Create an "Add a field" button + Publish the form

---
+++ formbuilder/components/builder/FormActions.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router";
+import AddFieldDropdown from "./AddFieldDropdown"
+import {Button, ButtonToolbar, ButtonGroup}  from "react-bootstrap"
+
+export default function FormActions(props) {
+  const onClick = (event) => {
+    props.publishForm(({collection, adminToken}) => {
+      props.history.pushState(null, `/builder/published/${adminToken}`);
+    });
+  };
+
+  let saveIconName;
+  if (props.status == "pending") {
+    saveIconName = "refresh spin";
+  } else {
+    saveIconName = "save";
+  }
+
+  return (
+    <div>
+      <ButtonToolbar className="builder-inner-actions">
+        <AddFieldDropdown className="pull-right" {...props} />
+      </ButtonToolbar>
+      <ButtonGroup className="pull-right">
+        <Button onClick={() => confirm("This action will reset all unsaved changes, Are you sure?") && props.resetForm()}>
+          <i className="glyphicon glyphicon-remove" />
+          Reset <span className="hidden-xs">form</span>
+        </Button>
+        <Button bsStyle="success" onClick={onClick}>
+          <i className={`glyphicon glyphicon-${saveIconName}`} />
+          Save your form
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
+}
