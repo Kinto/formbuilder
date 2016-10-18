dab76ac60a9c0ecfeb1cb1049fcb06166abcb3f8
Sahil Dua <sahildua2305@gmail.com>
2016-10-18 23:26:09 +0200

    Change title back to project name after submitting the form
    Fixes #130

--- formbuilder/components/RecordCreated.js
+++ formbuilder/components/RecordCreated.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import config from "../config";
 
 export default function RecordCreated(props) {
+
+	// Issue #130 - Change title back to project name after submitting the form
+	document.title = config.projectName;
+
   return (
     <div>
       <h3>Submitted!</h3>
