59690e0bbcc8b4b7cb894e171eaa830dbc481eec
Alexis Métaireau <alexis@notmyidea.org>
2016-10-19 17:52:27 +0200

    Remove external dependencies

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -45,7 +45,15 @@ module.exports = {
           path.join(__dirname, "formbuilder"),
           path.join(__dirname, "node_modules"),
         ],
-      }
+      },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      },
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
     ]
   }
 };
