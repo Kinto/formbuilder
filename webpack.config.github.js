247ffc9d04cfe039b8f6042a53b7cc290371ec88
Alexis Métaireau <alexis@notmyidea.org>
2016-11-06 19:49:12 +0100

    Restore the formbuilder path when publishing to github

--- webpack.config.github.js
+++ webpack.config.github.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "build"),
     filename: "bundle.js",
-    publicPath: "/"
+    publicPath: "/formbuilder/"
   },
   plugins: [
     new webpack.IgnorePlugin(/^(buffertools)$/), // unwanted "deeper" dependency
