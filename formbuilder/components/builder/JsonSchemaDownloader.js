c1fe74d79df1ac6b57831c04f9ff08aad1e49eb0
Adam Hamer <adam@eccosolutions.co.uk>
2019-03-06 14:49:46 +0000

    Include uiSchema in JSON download

--- formbuilder/components/builder/JsonSchemaDownloader.js
+++ formbuilder/components/builder/JsonSchemaDownloader.js
@@ -4,12 +4,19 @@ import btoa from "btoa";
 
 export default function JsonSchemaDownloader(props) {
   const filename = props.schema.title + ".json";
-  const fileContent = "data:application/json;base64," + btoa(JSON.stringify(props.schema));
+  const schemaFileContent = "data:application/json;base64," + btoa(JSON.stringify(props.schema));
+  const uiSchemaFileContent = "data:application/json;base64," + btoa(JSON.stringify(props.uiSchema));
 
   return (
-    <a className="list-group-item" download={filename} href={fileContent}>
-      <i className="glyphicon glyphicon-download" />&nbsp;
-        Download JSON
-    </a>
+    <div>
+      <a className="list-group-item" download={filename} href={schemaFileContent}>
+        <i className="glyphicon glyphicon-download" />&nbsp;
+          Download JSON schema
+      </a>
+      <a className="list-group-item" download={filename} href={uiSchemaFileContent}>
+          <i className="glyphicon glyphicon-download" />&nbsp;
+      Download JSON ui
+      </a>
+    </div>
   );
 }
